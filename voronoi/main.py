24e385930a182a7f089df0ef604f4f9250ca4b52
Matteo Tessarolo <matteo.tessarolo@unibas.ch>
2024-09-02 08:09:59 +0200

    Update

--- voronoi/main.py
+++ voronoi/main.py
@@ -1,6 +1,5 @@
 from PIL import Image
 import numpy as np
-from scipy.ndimage import gaussian_filter
 import matplotlib.pyplot as plt
 import matplotlib
 from tqdm import tqdm
@@ -8,22 +7,37 @@ from tqdm import tqdm
 matplotlib.use("tkagg")
 
 
-def gauss_fun(x, x0, sigma):
-    return np.exp(-np.power(x - x0, 2) / (2 * sigma**2)) / np.sqrt(2 * np.pi * sigma**2)
+def count_maxima(array: np.ndarray):
+    return np.sum(
+        np.logical_and(
+            np.logical_and(
+                np.logical_and(
+                    np.logical_and(
+                        np.logical_and(
+                            array[1:-1, 1:-1, 1:-1] > array[0:-2, 1:-1, 1:-1],
+                            array[1:-1, 1:-1, 1:-1] > array[2:, 1:-1, 1:-1],
+                        ),
+                        array[1:-1, 1:-1, 1:-1] > array[1:-1, 0:-2, 1:-1],
+                    ),
+                    array[1:-1, 1:-1, 1:-1] > array[1:-1, 2:, 1:-1],
+                ),
+                array[1:-1, 1:-1, 1:-1] > array[1:-1, 1:-1, 0:-2],
+            ),
+            array[1:-1, 1:-1, 1:-1] > array[1:-1, 1:-1, 2:],
+        )
+    )
 
 
-def generate_gaussians_set(sigma):
-    gaussian_set = np.zeros((256, 256))
-    for i in range(256):
-        gaussian_set[i, :] = gauss_fun(np.arange(256), i, sigma)
-    return gaussian_set
-
-
-def generate_gaussian_3d(ix, iy, iz, gaussian_set):
-    return np.tensordot(
-        np.tensordot(gaussian_set[ix, :], gaussian_set[iy, :], axes=0),
-        gaussian_set[iz, :],
-        axes=0,
+def padded_laplacian(array: np.ndarray):
+    padded_array = np.pad(array, ((1, 1), (1, 1), (1, 1)), "edge")
+    return (
+        6 * padded_array[1:-1, 1:-1, 1:-1]
+        - padded_array[0:-2, 1:-1, 1:-1]
+        - padded_array[2:, 1:-1, 1:-1]
+        - padded_array[1:-1, 0:-2, 1:-1]
+        - padded_array[1:-1, 2:, 1:-1]
+        - padded_array[1:-1, 1:-1, 0:-2]
+        - padded_array[1:-1, 1:-1, 2:]
     )
 
 
@@ -36,49 +50,21 @@ if __name__ == "__main__":
 
     rgb_hist = np.zeros((256, 256, 256))
 
+    np.random.rand
+
     for pix in tqdm(rgb_list):
         rgb_hist[pix[0], pix[1], pix[2]] += 1
 
-    blurred = np.copy(rgb_hist)
+    blurred_rgb_hist = np.copy(rgb_hist)
     while not flag_max_count_achieved:
-        # gaussian_set=generate_gaussians_set(blur_size)
-
-        max_count = np.sum(
-            np.logical_and(
-                np.logical_and(
-                    np.logical_and(
-                        np.logical_and(
-                            np.logical_and(
-                                blurred[1:-1, 1:-1, 1:-1] > blurred[0:-2, 1:-1, 1:-1],
-                                blurred[1:-1, 1:-1, 1:-1] > blurred[2:, 1:-1, 1:-1],
-                            ),
-                            blurred[1:-1, 1:-1, 1:-1] > blurred[1:-1, 0:-2, 1:-1],
-                        ),
-                        blurred[1:-1, 1:-1, 1:-1] > blurred[1:-1, 2:, 1:-1],
-                    ),
-                    blurred[1:-1, 1:-1, 1:-1] > blurred[1:-1, 1:-1, 0:-2],
-                ),
-                blurred[1:-1, 1:-1, 1:-1] > blurred[1:-1, 1:-1, 2:],
-            )
-        )
+        max_count = count_maxima(blurred_rgb_hist)
 
         if max_count <= 25:
             break
 
-        blurred = np.divide(blurred, np.sum(blurred))
+        blurred_rgb_hist = np.divide(blurred_rgb_hist, np.sum(blurred_rgb_hist))
 
-        pad_blurred = np.pad(blurred, ((1, 1), (1, 1), (1, 1)))
-        laplacian = (
-            6 * pad_blurred[1:-1, 1:-1, 1:-1]
-            - pad_blurred[0:-2, 1:-1, 1:-1]
-            - pad_blurred[2:, 1:-1, 1:-1]
-            - pad_blurred[1:-1, 0:-2, 1:-1]
-            - pad_blurred[1:-1, 2:, 1:-1]
-            - pad_blurred[1:-1, 1:-1, 0:-2]
-            - pad_blurred[1:-1, 1:-1, 2:]
-        )
-
-        blurred -= laplacian * 0.1
+        blurred_rgb_hist -= padded_laplacian(blurred_rgb_hist) * 0.1
 
         print(max_count)
 
