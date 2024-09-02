c91d85ea8d01e3f34048eed73a90e5bfc5f7ac1f
Matteo Tessarolo <matteo.tessarolo@unibas.ch>
2024-09-02 07:54:45 +0200

    Update

--- main.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-
-if __name__ == "__main__":
-    img = Image.open("picture.jpg")  # Can be many different formats.
-    rgb = img.load()
-    img_shape = img.size
-    print(img_shape)
-    print(rgb[0, 0])
